772e77afce1fedf65cea281bf40e0dc754e5d3cc
mislisdis <adisalisa1@gmail.com>
2024-12-10 08:12:20 +0300

    python operations
    These are the python operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+diff = number1 - number2
+prod = number1 * number2
+
+print(f"The addition of {number1} and {number2} is {sum}")
+print(f"The difference of {number1} and {number2} is {diff}")
+print(f"The product of {number1} and {number2} is {prod}")
